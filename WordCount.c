c11f6241fe337966daccbae8340d410f0b8d5001
LittleCai <2243157861@qq.com>
2020-05-31 13:51:54 +0800

    CaiShuangLian have a midification

--- WordCount.c
+++ WordCount.c
@@ -5,63 +5,65 @@
 #include<malloc.h>
 #include<io.h>
 #pragma warning(disable:4996)
+int CodeCount(char* p);
+int WordCount(char* p);
 int main(int argc, char* argv[]) {
     char bet;
     FILE* f;
+    //参数输入
     if (argc < 3 || argc>3)
     {
         printf("1.输入参数错误");
         return -1;
     }
-    f = fopen(argv[2],"r");
-    assert(f!=0);
-    if (strcmp(argv[1],"-c")==0)
-    {
+    f = fopen(argv[2], "r");
+    assert(f != 0);
+    if (strcmp(argv[1], "-c") == 0)
         bet = 'c';
-    }
     else if (strcmp(argv[1], "-w") == 0)
-    {
         bet = 'w';
-    }
-    else 
+    else
     {
         printf("2.输入参数错误");
         return -1;
     }
-    func(bet, argv[2]);
+    //打印结果
+    switch (bet) {
+    case 'c':printf("字符数：%d\n", WordCount(bet, argv[2])); break;
+    case 'w':printf("字符数：%d\n", CodeCount(bet, argv[2])); break;
+    default:break;
+    }
     return 0;
 }
-int func(char ch,char* p)
+int WordCount(char* p)//单词统计
 {
-    FILE* f=fopen(p,"r");
+    FILE* f = fopen(p, "r");
     char bet;
     int num = 0;
-    int is_word = 0;
-    if (ch == 'w')
+    while (!feof(f))
     {
-        while (!feof(f))
-        {
-            bet = fgetc(f);
-            if ((bet >= 'a' && bet <= 'z') || (bet >= 'A' && bet <= 'Z') || bet == '_')
-                is_word = 1;
-            else
-            {
-                num++;
-                is_word = 0;
-            }
-        }
-        printf("单词数：%d\n",num);
+        bet = fgetc(f);
+        if ((bet != ' ') && (bet != '\n') && (bet != '\t'))//判断是否是单词
+            num++;
     }
-    else if(ch =='c')
-    {
-        while (!feof(f))
-        {
-            bet = fgetc(f);
-            if ((bet==' ')||(bet=='\n')||(bet=='\t'))
-                num++;
-        }
-        printf("字符数：%d",num);
-    }
-    return 0;
+    return num;
 }
-
+int CodeCount(char* p)//字符统计
+{
+    FILE* f = fopen(p, "r");
+    char bet;
+    int num = 0;
+    int is_code = 0;//字符状态
+    while (!feof(f))
+    {
+        bet = fgetc(f);
+        if ((bet >= 'a' && bet <= 'z') || (bet >= 'A' && bet <= 'Z'))//判断是否是字符
+        {
+            is_code = 1;
+            num++;
+        }
+        else
+            is_code = 0;
+    }
+    return num;
+}
